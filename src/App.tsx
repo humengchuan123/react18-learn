2ec334cdee889b78e6bc5bbd05c7bb1c3dbdb3ba
humengchuan <531537052@qq.com>
2023-04-18 16:56:12 +0800

    first commit, init project demo

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+/*
+ * @Author: humengchuan 531537052@qq.com
+ * @Date: 2023-04-18 14:33:26
+ * @LastEditors: humengchuan 531537052@qq.com
+ * @LastEditTime: 2023-04-18 15:11:35
+ * @FilePath: \react-learn-manager\src\App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      顶级组件
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </div>
+  )
+}
+
+export default App
