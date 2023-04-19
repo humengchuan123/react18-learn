a3c24500ad1706971fec9d0149bcbb08628bb9de
humengchuan <531537052@qq.com>
2023-04-19 15:10:34 +0800

    feat: 增加路由设置

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,13 @@
  * @Author: humengchuan 531537052@qq.com
  * @Date: 2023-04-19 09:26:32
  * @LastEditors: humengchuan 531537052@qq.com
- * @LastEditTime: 2023-04-19 09:37:37
+ * @LastEditTime: 2023-04-19 12:41:50
  * @FilePath: \react-learn-manager\src\router\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+// 路由懒加载
+import react, { lazy } from 'react'
+// const Home = lazy(() => import('@/views/Home'))
 import App from '../App'
 import Home from '@/views/Home'
 import About from '@/views/About'
@@ -16,6 +19,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
     // 内部有逻辑写法
 //     return()
 // }
+// 路由懒加载函数
+const lazyLoadingComponent = (component: JSX.Element) => (
+    <react.Suspense fallback={ <div>Loading...</div> }>
+        { component }
+    </react.Suspense>
+)
 const baseRouter = () => (
     <BrowserRouter>
         <Routes>
